20501675dd71a0bdd86f945378ac927422f0dea7
Abdo_Halem10 <abdohalem305@gmail.com>
2024-12-14 05:15:34 +0200

    Updated the publisher demo

--- Socket_Codes/MQTT/Demo_Codes/publisher_demo.py
+++ Socket_Codes/MQTT/Demo_Codes/publisher_demo.py
@@ -3,8 +3,6 @@ import paho.mqtt.publish as publish
 MQTT_SERVER = "test.mosquitto.org"
 MQTT_PATH = "ADAS_GP/sign"
 
-
-
 while True:
     msg = input("Enter the message: ")
     publish.single(MQTT_PATH, msg, hostname=MQTT_SERVER)
