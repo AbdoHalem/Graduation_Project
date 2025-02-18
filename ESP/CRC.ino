842c531b6304a364aa88c795fdce9b9a1b2e117e
Mohamed ElSayed <mohamedelsayedmahmoud21@gmail.com>
2025-02-18 20:17:57 +0200

    Add files via upload

---
+++ ESP/CRC.ino
@@ -0,0 +1,47 @@
+
+// this crc polynomial is like it "that is for example .. "
+const uint16_t crc_polynomial = 0x8008;
+
+uint16_t calculateCRC(const uint8_t* data, size_t length) {
+    uint16_t crc = 0;
+
+    for (size_t i = 0; i < length; ++i) {
+        crc ^= (uint16_t)data[i] << 8;
+
+        for (int j = 0; j < 8; ++j) {
+            if (crc & 0x8000) {
+                crc = (crc << 1) ^ crc_polynomial;
+            } else {
+                crc = crc << 1;
+            }
+        }
+    }
+
+    return crc;
+}
+
+void setup() {
+    Serial.begin(9600);
+    
+       // set your protocol and recieve the data form the protocol .. 
+    
+
+      // i simulate the recieved data .. using this array  
+    uint8_t data[] = {0x12, 0x34, 0x56, 0x78};
+
+    
+    uint16_t crcChecksum = calculateCRC(data, sizeof(data));
+     // make a comperison between the original data and crc output .. 
+    Serial.print("Data: ");
+    for (size_t i = 0; i < sizeof(data); ++i) {
+        Serial.print(data[i], HEX);
+        Serial.print(" ");
+    }
+    Serial.print("\nCRC Checksum: 0x");
+    Serial.println(crcChecksum, HEX);
+
+}
+
+void loop() {
+    
+}
